d3f48c21c1343d321d2a20fc740a7fc3d637b791
Li Tian <1518061368@qq.com>
2025-04-21 03:38:50 +0800

    create tab1.page.ts
    tab1 is about add item

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpService } from '../service/http.service';
 
 @Component({
-  selector: 'app-add',
-  templateUrl: 'add.page.html',
-  styleUrls: ['add.page.scss'],
-  standalone: false,
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class AddPage {
+export class Tab1Page implements OnInit, OnDestroy {
+    private reloadListener: any;
 
-  constructor() {}
+    constructor(private http: HttpService) {
+        // Initialize the reload listener
+        this.reloadListener = () => {
+            this.getAll();
+        };
+    }
 
+    goods = []
+    inputModel = ''
+
+    ngOnInit() {
+        window.addEventListener('reload-tab1', this.reloadListener);
+        this.getAll();
+    }
+
+    ngOnDestroy() {
+        window.removeEventListener('reload-tab1', this.reloadListener);
+    }
+
+    search() {
+        this.goods = [];
+        this.getAll();
+    }
+
+    getAll() {
+        this.http.getAll(this.inputModel)
+            .subscribe((res: any) => {
+                console.log(res)
+                this.goods = res;
+            })
+    }
 }
