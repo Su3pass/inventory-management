8dbed8a378581863637ce2cbb42b6ec71142b59f
Gao2024-sys <j.gao.25@student.scu.edu.au>
2025-04-21 02:50:22 +0800

    The featured product page has been modified and a deletion function has been added.
    The featured product page of the system library has been changed and the function of deleting items by name has been added.

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,19 +1,51 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../service/http.service';
 
-import { TabsPageRoutingModule } from './tabs-routing.module';
 
-import { TabsPage } from './tabs.page';
-
-@NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    TabsPageRoutingModule
-  ],
-  declarations: [TabsPage]
+@Component({
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
-export class TabsPageModule {}
+export class Tab2Page {
+    featured = [];
+    isModalOpen = false;
+    itemName = '';
+    category = '';
+    quantity = '';
+    price = '';
+    supplierName = '';
+    stockStatus = '';
+    featuredItem = '';
+    specialNote = '';
+    constructor(private http: HttpService) { }
+    ngOnInit() {
+        this.getFeatured();
+    }
+    getFeatured(){
+        this.http.getAll('').subscribe((res: any)=>{
+            this.featured = res.filter(item=>item.featured_item === 1);
+        })
+    }
+    showModal(){
+        this.isModalOpen = true;
+    }
+    create(){
+        this.http.create({
+            item_name: this.itemName,
+            category: this.category,
+            quantity: this.quantity,
+            price: this.price,
+            supplier_name: this.supplierName,
+            stock_status: this.stockStatus,
+            featured_item: this.featuredItem,
+            special_note: this.specialNote,
+        }).subscribe((res: any)=>{
+            this.getFeatured();
+            this.isModalOpen = false;
+        })
+    }
+    setOpen(isOpen: boolean) {
+        this.isModalOpen = isOpen;
+    }
+}
