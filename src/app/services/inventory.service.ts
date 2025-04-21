593a7a0527f8390eef4baff0e13757ea035cc800
Gao2024-sys <j.gao.25@student.scu.edu.au>
2025-04-21 13:20:29 +0800

    Redefine the data model
    The definition of the data model has been redefined and modified。

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,41 +1,23 @@
 import { Injectable } from '@angular/core';
-import { InventoryItem } from '../models/inventory-item.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class InventoryService {
-  private apiUrl = 'https://prog2005.it.scu.edu.au/ArtGalley';
-  
-  constructor() { }
+export class HttpService {
 
-  // 获取所有项目
-  getAllItems(): Promise<InventoryItem[]> {
-    // 实际实现将调用API
-    return Promise.resolve([]);
-  }
-
-  // 添加新项目
-  addItem(item: InventoryItem): Promise<InventoryItem> {
-    // 实际实现将调用API
-    return Promise.resolve(item);
-  }
-
-  // 更新项目
-  updateItem(name: string, item: InventoryItem): Promise<InventoryItem> {
-    // 实际实现将调用API
-    return Promise.resolve(item);
-  }
-
-  // 删除项目
-  deleteItem(name: string): Promise<void> {
-    // 实际实现将调用API
-    return Promise.resolve();
-  }
-
-  // 获取特色项目
-  getFeaturedItems(): Promise<InventoryItem[]> {
-    // 实际实现将调用API
-    return Promise.resolve([]);
-  }
+    constructor(private http: HttpClient) { }
+    url = 'https://prog2005.it.scu.edu.au/ArtGalley';
+    getAll(data) {
+        return this.http.get(this.url+'/'+data);
+    }
+    create(data){
+        return this.http.post(this.url, data);
+    }
+    delete(data){
+        return this.http.delete(this.url+'/'+data);
+    }
+    update(name: string, data: any) {
+        return this.http.put(`${this.url}/${name}`, data);
+    }
 }
